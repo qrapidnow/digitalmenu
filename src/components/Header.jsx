310fa900e1be51964ccadf085651552f1347305e
qrapidnow <qrapidnow@gmail.com>
2024-07-11 11:30:03 +0530

    details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ restaurantName }) => {
     <header className="header">
       <h1 className="header-title">{restaurantName || "Kitchen Stories"}</h1>
       <p className="header-subtitle">Prabhadevi, Mumbai</p>
-      <p className="header-status">Open. 12:00 PM - 4:00 PM / 7:00 PM - 12:00 AM</p>
+      <p className="header-status">12:00 PM - 4:00 PM / 7:00 PM - 12:00 AM</p>
       <div className="header-buttons">
 
       </div>

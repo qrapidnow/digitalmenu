aae7342dc4f2f789d78110ace0f75e06cb5e6d27
qrapidnow <qrapidnow@gmail.com>
2024-07-12 09:13:59 +0530

    Search-Engine

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = ({ setSearchTerm }) => {
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search in menu" className="search-bar-input" />
+      <input 
+        type="text" 
+        placeholder="Search in menu" 
+        className="search-bar-input"
+        onChange={handleInputChange} 
+      />
     </div>
   );
 };

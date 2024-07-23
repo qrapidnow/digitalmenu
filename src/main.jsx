6d0a518c81b5f09d2f4c3c675c35518c4d7c8d03
qrapidnow <qrapidnow@gmail.com>
2024-07-23 21:37:10 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Router>
     <Routes>
       <Route path="/:userId" element={<App />} />
     </Routes>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );

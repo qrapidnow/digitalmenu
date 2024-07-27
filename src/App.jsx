1cf6f4a5906474afc2410192d07fc3d50cc1f78c
qrapidnow <qrapidnow@gmail.com>
2024-07-27 19:56:41 +0530

    fix spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';

16c9341ca6c912ffdb837a997af0df708ddd2e73
qrapidnow <qrapidnow@gmail.com>
2024-08-03 20:26:18 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the warning limit from 500 kBs to 1000 kBs
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          if (id.includes('src/components')) {
-            return 'components';
-          }
-          // You can add more conditions for other chunks if needed
-        },
-      },
-    },
-  },
 });

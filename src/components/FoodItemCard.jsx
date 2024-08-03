059c10fd73cdfb86f33e9e59e8a99fb542f7507f
qrapidnow <qrapidnow@gmail.com>
2024-08-03 17:11:44 +0530

    updating  design of navbar

--- src/components/FoodItemCard.jsx
+++ src/components/FoodItemCard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FoodItemCard.css';
 
 const FoodItemCard = ({ item, addItem }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const handleAdd = (e) => {
     e.stopPropagation();
     console.log('Adding item:', item);
     addItem(item);
+  };
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
   };
 
   return (
@@ -15,7 +21,14 @@ const FoodItemCard = ({ item, addItem }) => {
         <div className="food-item-details">
           <h3 className="food-item-title">{item.name}</h3>
           <p className="food-item-price">₹{item.price}</p>
-          <p className="food-item-description">{item.description}</p>
+          <p className="food-item-description">
+            {isExpanded ? item.description : `${item.description.substring(0, 50)}...`}
+            {item.description.length > 50 && (
+              <span className="read-more" onClick={toggleDescription}>
+                {isExpanded ? ' Show Less' : ' Read More'}
+              </span>
+            )}
+          </p>
           <button onClick={handleAdd} className="add-button">Add</button>
         </div>
       </div>

ed22f6ccef5eba02590bcf5563374f6f62fc0d15
qrapidnow <qrapidnow@gmail.com>
2024-08-09 19:15:32 +0530

    loading..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = ({ restaurantName }) => {
   return (
     <header className="header">
-      <h1 className="header-title">{restaurantName || "Kitchen Stories"}</h1>
+      <h1 className="header-title">{restaurantName || "Loading..."}</h1>
       <p className="header-subtitle"> Kharghar, Navi Mumbai</p>
       <p className="header-status">11:30 AM - 3:30 PM / 7:30 PM - 11:30 PM</p>
     </header>

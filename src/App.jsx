e46f8cfa41a9e33b4a017072789cf6c71efd29bd
qrapidnow <qrapidnow@gmail.com>
2024-08-09 13:26:19 +0530

    refresh fix + storing

--- src/App.jsx
+++ src/App.jsx
@@ -99,12 +99,11 @@ const App = () => {
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         try {
-            // Save customer details along with the digital menu (restaurantName) to Firestore
+            // Save customer details to Firestore
             await addDoc(collection(db, "customer-details"), {
                 name: customerName,
                 whatsapp_number: whatsappNumber,
                 timestamp: new Date(),
-                digital_menu: restaurantName,  // Including the digital menu or restaurant name
             });
             setIsFormSubmitted(true);
             saveCartData();

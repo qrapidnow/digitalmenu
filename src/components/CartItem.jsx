1b7b45225a15df69661f5ad6b2c239c9cf59fc9f
qrapidnow <qrapidnow@gmail.com>
2024-08-09 19:46:18 +0530

    SIZE OF THE BACK

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -16,19 +16,19 @@ const CartItem = ({ cartItems, setShowCartItem, updateItemCount, removeItem }) =
     const storedCustomerData = JSON.parse(localStorage.getItem('customerData'));
     const currentTime = new Date().getTime();
 
-    // if (storedCartData && storedCustomerData) {
-    //   // Check if the stored data is within the 20-minute limit
-    //   if (currentTime - storedCartData.timestamp < 20 * 60 * 1000) {
-    //     setShowListPage(true);
-    //     setCustomerName(storedCustomerData.name);
-    //     setWhatsappNumber(storedCustomerData.whatsapp_number);
-    //     setIsFormSubmitted(true);
-    //   } else {
-    //     // Clear expired data
-    //     localStorage.removeItem('cartData');
-    //     localStorage.removeItem('customerData');
-    //   }
-    // }
+    if (storedCartData && storedCustomerData) {
+      // Check if the stored data is within the 20-minute limit
+      if (currentTime - storedCartData.timestamp < 20 * 60 * 1000) {
+        setShowListPage(true);
+        setCustomerName(storedCustomerData.name);
+        setWhatsappNumber(storedCustomerData.whatsapp_number);
+        setIsFormSubmitted(true);
+      } else {
+        // Clear expired data
+        localStorage.removeItem('cartData');
+        localStorage.removeItem('customerData');
+      }
+    }
   }, []);
 
   const saveCartData = () => {

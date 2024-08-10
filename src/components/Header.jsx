b19b34b3ae6c76f030380093f530512ae5bcecd1
qrapidnow <qrapidnow@gmail.com>
2024-08-10 16:47:26 +0530

    navi mumbai

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ restaurantName }) => {
   return (
     <header className="header">
       <h1 className="header-title">{restaurantName || "Loading..."}</h1>
-      <p className="header-subtitle"> Kharghar, Navi Mumbai</p>
+      <p className="header-subtitle">Navi Mumbai</p>
       <p className="header-status">11:30 AM - 3:30 PM / 7:30 PM - 11:30 PM</p>
     </header>
   );

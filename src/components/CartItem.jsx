d1ac100fdb5d42a957cb2b5da9726bbd9a76e82d
qrapidnow <qrapidnow@gmail.com>
2024-08-11 20:08:55 +0530

    res name

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -70,14 +70,14 @@ const CartItem = ({ cartItems, setShowCartItem, updateItemCount, removeItem, set
             });
             setIsFormSubmitted(true);
             saveCartData();
-            setShowCustomerForm(false); // Hide customer form after submission
-            setShowListPage(false); // Ensure the list page is hidden
-            setShowCartItem(true); // Show cart items after form submission
+            setShowCustomerForm(false); // Hide the customer form
+            setShowCartItem(true); // Show the cart immediately after form submission
         } catch (error) {
             console.error("Error adding document: ", error);
             alert("There was an error saving your information. Please try again.");
         }
     };
+    
 
     if (showCustomerForm && !isFormSubmitted) {
         return (

4c6fe97ea494c72652d9733b8dfad398c7e47672
qrapidnow <qrapidnow@gmail.com>
2024-08-17 11:09:23 +0530

    ony table

--- src/components/PlaceOrderPage.jsx
+++ src/components/PlaceOrderPage.jsx
@@ -4,8 +4,6 @@ import Swal from 'sweetalert2';
 import './PlaceOrderPage.css';
 
 const PlaceOrderPage = ({ cartItems, setShowPlaceOrderPage }) => {
-  const [name, setName] = useState('');
-  const [whatsapp, setWhatsapp] = useState('');
   const [tableNo, setTableNo] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -14,8 +12,6 @@ const PlaceOrderPage = ({ cartItems, setShowPlaceOrderPage }) => {
     setIsLoading(true);
 
     const orderData = {
-      name,
-      whatsapp,
       tableNo,
       items: cartItems.map(item => ({
         name: item.name,
@@ -43,7 +39,6 @@ const PlaceOrderPage = ({ cartItems, setShowPlaceOrderPage }) => {
     } catch (error) {
       console.error('Error sending order:', error);
       if (error.response) {
-        // Log details of response error if it exists
         console.error('Response data:', error.response.data);
         console.error('Response status:', error.response.status);
       }
@@ -77,27 +72,6 @@ const PlaceOrderPage = ({ cartItems, setShowPlaceOrderPage }) => {
             value={tableNo}
             onChange={(e) => setTableNo(e.target.value)}
           />
-
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-
-          <label htmlFor="whatsapp">WhatsApp Number:</label>
-          <input
-            type="text"
-            id="whatsapp"
-            name="whatsapp"
-            required
-            value={whatsapp}
-            onChange={(e) => setWhatsapp(e.target.value)}
-          />
-
           <button type="submit" className="place-order-button">Submit Order</button>
         </form>
       )}
